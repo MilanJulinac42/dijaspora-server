6975a19cbf4730befbe8cdc4731d6cd4e939563e
milan julinac <milanjulinac996@gmail.com>
2024-02-21 15:27:39 +0100

    added typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       autoSchemaFile: 'schema.gql',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'milanjulinac',
+      password: '',
+      database: 'dijaspora',
+      entities: ['dist/**/*.model.js'],
+      synchronize: false,
     }),
   ],
   controllers: [AppController],
